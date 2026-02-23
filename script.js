d5a92c5ea97871fb78e65a26acbcb1420281f072
Le-Lucas <56552953+Le-Lucas@users.noreply.github.com>
2026-02-23 08:25:27 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -154,12 +154,17 @@ function loadSaveData() {
 
 function bootTerminal() {
     loadSaveData();
-    const btnStart = document.getElementById("btn-start");
+        const btnStart = document.getElementById("btn-start");
     if(btnStart) btnStart.onclick = () => { 
-        document.getElementById("start-screen").classList.remove("active"); document.getElementById("hero-screen").classList.add("active"); 
+        
+        window.ativarModoImersivo(); // 👈 A INJEÇÃO DA TELA CHEIA VOLTOU AQUI!
+        
+        document.getElementById("start-screen").classList.remove("active"); 
+        document.getElementById("hero-screen").classList.add("active"); 
         playSound("click"); playSound("bgm"); 
         document.getElementById("fragment-count").innerText = playerFragments;
     };
+
     if(document.getElementById("btn-home")) document.getElementById("btn-home").onclick = () => location.reload();
 
     document.querySelectorAll(".hero-choice").forEach(choice => {
